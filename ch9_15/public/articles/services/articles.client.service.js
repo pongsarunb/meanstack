0c27987e0b46ebc61972cf5d29d0a01eeb4f5677
Tun Pongsarun <pongsarunb@gmail.com>
2019-07-15 19:30:21 +0700

    Add files via upload

---
+++ ch9_15/public/articles/services/articles.client.service.js
@@ -0,0 +1,10 @@
+angular.module('articles').factory('Articles', ['$resource', function ($resource) {
+    // Use the '$resource' service to return an article '$resource' object
+    return $resource('api/articles/:articleId', {
+        articleId: '@_id'
+    }, {
+            update: {
+                method: 'PUT'
+            }
+        });
+}]);
